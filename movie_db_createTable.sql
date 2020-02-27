fcad111d494f0b468a6073c6f03bc9e9f8ed71a1
Muonaka Anthony <40110972+Antonynans@users.noreply.github.com>
2020-02-27 17:55:12 +0100

    movie_db_createTable

--- movie_db_createTable.sql
+++ movie_db_createTable.sql
@@ -1,18 +1,18 @@
 use movie_db;
 
 CREATE TABLE Movie (
-	movieId					integer			NOT NULL,
+    movieId				integer			NOT NULL,
     movieTitle				char(50)		NOT NULL,
     movieYear				integer			NOT NULL,
     movieTime				integer			NULL,
     movieLanguage			char(50)		NOT NULL,
-    movieDateReleased		date			NOT NULL,
-    movieReleaseCountry		char(5)			NOT NULL,
+    movieDateReleased			date			NOT NULL,
+    movieReleaseCountry			char(5)			NOT NULL,
     CONSTRAINT				movie_PK		PRIMARY KEY(MovieId)
     );
     
 CREATE TABLE Actor(
-    ActorId				Integer			NOT NULL,
+    ActorId			Integer			NOT NULL,
     ActorFirstNmae		char(50)		NOT NULL,
     ActorLastName		char(50)		NOT NULL,
     ActorGender			char(1)			NOT NULL,
@@ -20,37 +20,37 @@ CREATE TABLE Actor(
 );
     
 CREATE TABLE MovieCast (
-	ActorId			Integer				NOT NULL,
+    ActorId			Integer				NOT NULL,
     MovieId			Integer				NOT NULL,
     Role			char(30)			NOT NULL,
-    CONSTRAINT		MovieCast_FK		PRIMARY KEY(MovieId,ActorId),
-    CONSTRAINT		Actor_PK			FOREIGN KEY(ActorId)
+    CONSTRAINT			MovieCast_FK			PRIMARY KEY(MovieId,ActorId),
+    CONSTRAINT			Actor_PK			FOREIGN KEY(ActorId)
 					REFERENCES			Actor(ActorId),
-    CONSTRAINT		Movie_PK			FOREIGN KEY(movieId)
+    CONSTRAINT			Movie_PK			FOREIGN KEY(movieId)
 					REFERENCES			Movie(MovieId)
 );
 
 CREATE TABLE Director(
-	DirectorId			Integer			NOT NULL,
-    DirectorFirstName	char(20)		NOt NULL,
-    DirectorLastName	char(20)		NOT NULL,
+    DirectorId			Integer			NOT NULL,
+    DirectorFirstName		char(20)		NOt NULL,
+    DirectorLastName		char(20)		NOT NULL,
     CONSTRAINT			Director_PK		PRIMARY KEY	(DirectorId)
 );
 
 CREATE TABLE MovieDirection (
-	DirectorId			Integer					NOT NULL,
-    MovieId				Integer					NOT NULL
+    DirectorId			Integer					NOT NULL,
+    MovieId			Integer					NOT NULL
   /*  CONSTRAINT 		MovieDirection_PK		PRIMARY KEY(DirectiorId, MovieId)*/
 );
 
 CREATE TABLE Reviewer (
-	ReviewerId			integer 		NOT NULL,
+    ReviewerId			integer 		NOT NULL,
     ReviewerName		char(30)		NOT NULL,
     CONSTRAINT			Reviewer_PK		PRIMARY KEY(ReviewerId)
     );
     
 CREATE TABLE Rating (
-	MovieId				integer				not null,
+    MovieId			integer				not null,
     reviewerId			integer				not null,
     reviewerStars		integer				not null,
     Num_O_rating		integer				not null,
@@ -62,9 +62,9 @@ CREATE TABLE Rating (
 	);
     
 CREATE TABLE  Genres (
-	genId			integer				not null,
-    genTitle		char(20)			null,
-    CONSTRAINT		 genres_PK			PRIMARY KEY	(genId)
+    genId			integer				not null,
+    genTitle			char(20)			null,
+    CONSTRAINT		 	genres_PK			PRIMARY KEY	(genId)
     );
 	
     

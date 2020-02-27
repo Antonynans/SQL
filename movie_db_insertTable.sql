a25418289a5c913e60fe6046112a840e829ea234
Muonaka Anthony <40110972+Antonynans@users.noreply.github.com>
2020-02-27 18:00:16 +0100

    Add files via upload

---
+++ movie_db_insertTable.sql
@@ -0,0 +1,59 @@
+INSERT INTO Movie VALUES (
+'001', 'Jumanji', '2019', '1230', 'English', '2019-01-01', 'USA');
+INSERT INTO Movie VALUES (
+'002', 'Spies in Disguise', '2019', '1045', 'English', '2019-01-01', 'USA');
+INSERT INTO Movie VALUES (
+'003', 'Your Excellency', '2020', '1220', 'English', '2020-01-01', 'Ngn');
+INSERT INTO Movie VALUES (
+'004', 'American Beauty', '1999', '1220', 'English', '1999-01-01', 'Ngn');
+INSERT INTO Movie VALUES (
+'005', 'Eyes Wide Shut', '1999', '1230', 'English', '1999-01-01', 'USA');
+
+INSERT INTO Actor VALUES (
+'201', 'Dwanyne', 'Johnson', 'm');
+INSERT INTO Actor VALUES (
+'202', 'Will', 'Smith', 'm');
+INSERT INTO Actor VALUES (
+'203', 'Bimbo', 'Manuel', 'f');
+
+INSERT INTO MovieCast VALUES (
+'201', '001', 'Dr. Bravestone');
+INSERT INTO MovieCast VALUES (
+'202', '002', 'spy');
+INSERT INTO MovieCast VALUES (
+'203', '003', 'party agent');
+
+INSERT INTO Director VALUES (
+'301', 'John', 'Dow');
+INSERT INTO Director VALUES (
+'302', 'Mark', 'Spencer');
+INSERT INTO Director VALUES (
+'303', 'Kemi', 'Ola');
+
+INSERT INTO MovieDirection VALUES (
+'301', '001');
+INSERT INTO MovieDirection VALUES (
+'302', '002');
+INSERT INTO MovieDirection VALUES (
+'303', '003');
+
+INSERT INTO Reviewer VALUES (
+'401', 'James');
+INSERT INTO Reviewer VALUES (
+'402', 'Tony');
+INSERT INTO Reviewer VALUES (
+'403', 'Alice');
+
+INSERT INTO Rating VALUES (
+'001', '401', '4', '80');
+INSERT INTO Rating VALUES (
+'002', '402', '4', '90');
+INSERT INTO Rating VALUES (
+'003', '403', '3', '75');
+
+INSERT INTO Genres VALUES (
+'501', 'Adventure');
+INSERT INTO Genres VALUES (
+'502', 'Sci-fic');
+INSERT INTO Genres VALUES (
+'503', 'Comedy');
